7376035e5a301d254c20c33f0b3de71065eaaeba
dnth <dickson.neoh@gmail.com>
2025-03-09 13:50:15 +0800

    make into package in pixi

---
+++ src/deim_wrapper/__init__.py
@@ -0,0 +1,11 @@
+from .engine import optim
+from .engine import data
+from .engine import deim
+
+from .engine.backbone import *
+
+from .engine.backbone import (
+    get_activation,
+    FrozenBatchNorm2d,
+    freeze_batch_norm2d,
+)
