546bfbf734456ee81f7c0d814e8905495b4bc4f1
Dickson Neoh <dickson.neoh@gmail.com>
2025-03-10 13:23:39 +0800

    Project Structure and Initial Setup (#1)
    * make into package in pixi
    
    * fist working version of predictor class
    
    * clean up
    
    * update
    
    * update
    
    * update
    
    * add support for custom model
    
    * rename project
    
    * add new asset images
    
    * update badges
    
    * add logo
    
    * update
    
    * updat eteip
    
    * update
    
    * update pixi ins
    
    * remove fiels
    
    * p

---
+++ src/deimkit/__init__.py
@@ -0,0 +1,15 @@
+from .engine import optim
+from .engine import data
+from .engine import deim
+
+from .engine.backbone import *
+
+from .engine.backbone import (
+    get_activation,
+    FrozenBatchNorm2d,
+    freeze_batch_norm2d,
+)
+
+from .config import Config, list_models
+from .predictor import load_model, Predictor
+from .visualization import visualize_detections, draw_on_image
